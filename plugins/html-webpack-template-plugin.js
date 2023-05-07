835daa187c44a18e244aaa3bd5576d43c2059840
Cheng Huang <cheng@duck.com>
2023-05-07 21:35:32 +0800

    build: change the event of html webpack template plugin

--- plugins/html-webpack-template-plugin.js
+++ plugins/html-webpack-template-plugin.js
@@ -6,19 +6,16 @@ class HtmlWebpackTemplatePlugin {
   }
   apply(compiler) {
     compiler.hooks.compilation.tap('HtmlWebpackTemplatePlugin', (compilation) => {
-      HtmlWebpackPlugin.getHooks(compilation).beforeAssetTagGeneration.tapAsync(
-        'HtmlWebpackTemplatePlugin',
-        (data, cb) => {
-          this.options
-            .func(data)
-            .then((resp) => {
-              cb(null, resp);
-            })
-            .catch(() => {
-              cb(null, data);
-            });
-        },
-      );
+      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync('HtmlWebpackTemplatePlugin', (data, cb) => {
+        this.options
+          .func(data)
+          .then((resp) => {
+            cb(null, resp);
+          })
+          .catch(() => {
+            cb(null, data);
+          });
+      });
     });
   }
 }
